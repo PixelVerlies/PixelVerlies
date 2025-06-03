5d3ea729652b698e91935586c9ddfa1da4e4621e
GeisterEule <140849816+GeisterEule@users.noreply.github.com>
2025-06-03 10:00:08 +0200

    changed main

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 import grid
-import textFunctions
-import login_screen
-import registration_screen
-import menu_screen
 import sys
+from database import database
+import dungeonEnd
+import map
+import createDungeon
 
+
+data = database()
+data.connection()
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -23,46 +26,38 @@ ueberschrift = pygame.font.Font("Arcade-Classic-Font/bytebounce.medium.TTF", 45)
 textKoerper = pygame.font.Font("Arcade-Classic-Font/bytebounce.medium.TTF", 25)
 
 blockSize = 25
-fild_leng = WIDTH / blockSize
-fild_high = HEIGHT / blockSize
+fild_leng = int(WIDTH / blockSize)
+fild_high = int(HEIGHT / blockSize)
 
-# 1 = Login, 2 = Registration, 3 = Main Menu
 site = 1
 
 run = True
+wall = grid.importImage("Images/Dungeon/wall.png", blockSize)
 
-# In der Hauptschleife:
-login_data = login_screen.create_login_fields(ueberschrift, textKoerper)
-registration_data = registration_screen.create_registration_fields(ueberschrift, textKoerper)
-menu_data = menu_screen.create_menu_fields(ueberschrift, textKoerper)
+rod, charac, door_list, all_rooms = createDungeon.create(data, blockSize, fild_leng, fild_high, 1)
+
+counter = 90
+
 
 while run:
-    for event in pygame.event.get():
+    events = pygame.event.get()
+
+    for event in events:
         if event.type == pygame.QUIT: 
             run = False
-        
-        if site == 1:  # Login screen
-            site = login_screen.handle_login_events(event, login_data, site)
-        elif site == 2:  # Registration screen
-            site = registration_screen.handle_registration_events(event, registration_data, site)
-        elif site == 3:  # Main menu
-            site = menu_screen.handle_menu_events(event, menu_data, site)
-    
-    SCREEN.fill(BLACK)
-    grid.drawGrid(WIDTH, HEIGHT, blockSize, SCREEN) 
 
-    if site == 1:
-        for field in login_data["fields"]:
-            field.drawField(blockSize, SCREEN)
-    elif site == 2:
-        for field in registration_data["fields"]:
-            field.drawField(blockSize, SCREEN)
-    elif site == 3:
-        for field in menu_data["fields"]:
-            field.drawField(blockSize, SCREEN)
+    SCREEN.fill(BLACK)
+    grid.drawGrid(WIDTH, HEIGHT, blockSize, SCREEN, wall)
+
+    if rod.end == 0:
+        rod.roundRun(events, charac, door_list, all_rooms)
+        map.drawGamefild(rod, charac, SCREEN, blockSize, textKoerper)
+    else:
+        counter, run = dungeonEnd.dungeonEnd(rod, fild_leng, fild_high, SCREEN, blockSize, ueberschrift, counter, data, charac)
 
     pygame.display.flip()
     clock.tick(60)
 
+data.connClose()
 pygame.quit()
 sys.exit()
